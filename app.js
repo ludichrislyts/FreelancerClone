fa6975211d0c604aeed32bd9b0be6b44432425f6
Kyle Pratuch & Chris Lytsell <student@epicodus.com>
2015-09-29 15:07:29 -0700

    add alternate view to upper body for 'hire'

--- app.js
+++ app.js
@@ -9,13 +9,44 @@ freelancerClone.config(function($stateProvider, $urlRouterProvider){
         templateUrl: 'partials/header.html',
         // controller: 'HeadersCtrl',
       },
+      'upper-body':{
+        templateUrl:'partials/hero.html',
+        controller: 'ImagesCtrl',
+      },
       'body':{
         templateUrl:'partials/home.html',
-        controller: 'ImagesCtrl'
+        controller: 'ImagesCtrl',
       },
+      // 'home.home-splash':{
+      //   templateUrl: 'partials/hero.html',
+      // },
       'footer':{
         templateUrl: 'partials/footer.html',
+      }
+    }
+  });
+
+  $stateProvider.state('hire', {
+    url:"/hire",
+    views:{
+      'header':{
+        templateUrl: 'partials/header.html',
+        // controller: 'HeadersCtrl',
       },
+      'upper-body':{
+        templateUrl:'partials/hire.html',
+        controller: 'ImagesCtrl',
+      },
+      'body':{
+        templateUrl:'partials/home.html',
+        controller: 'ImagesCtrl',
+      },
+      // 'home.home-splash':{
+      //   templateUrl: 'partials/hero.html',
+      // },
+      'footer':{
+        templateUrl: 'partials/footer.html',
+      }
     }
   });
 });
